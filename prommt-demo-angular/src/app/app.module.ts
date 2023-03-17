e812d4fd541d07442f22edc92e23c3790d071ff5
Kiz09 <felix.mellado.sdm@gmail.com>
2023-03-17 20:31:59 +0100

    Improvements and Observables properly implemented

--- prommt-demo-angular/src/app/app.module.ts
+++ prommt-demo-angular/src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaymentsListComponent } from './components/payments-list/payments-list.component';
-import {HttpClientModule} from "@angular/common/http";
-import {TableModule} from "primeng/table";
-import {ToolbarModule} from "primeng/toolbar";
-import {ButtonModule} from "primeng/button";
-import {ReactiveFormsModule} from "@angular/forms";
-import {DropdownModule} from "primeng/dropdown";
-import {RippleModule} from "primeng/ripple";
-import {InputTextModule} from "primeng/inputtext";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {CommonModule} from "@angular/common";
-import {CalendarModule} from "primeng/calendar";
+import { HttpClientModule } from "@angular/common/http";
+import { TableModule } from "primeng/table";
+import { ToolbarModule } from "primeng/toolbar";
+import { ButtonModule } from "primeng/button";
+import { ReactiveFormsModule } from "@angular/forms";
+import { DropdownModule } from "primeng/dropdown";
+import { RippleModule } from "primeng/ripple";
+import { InputTextModule } from "primeng/inputtext";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CommonModule } from "@angular/common";
+import { CalendarModule } from "primeng/calendar";
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { DialogModule } from 'primeng/dialog';
 
 @NgModule({
   declarations: [
@@ -33,9 +36,11 @@ import {CalendarModule} from "primeng/calendar";
     InputTextModule,
     BrowserAnimationsModule,
     CommonModule,
-    CalendarModule
+    CalendarModule,
+    ToastModule,
+    DialogModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- prommt-demo-angular/src/app/components/payments-list/payments-list.component.ts
+++ prommt-demo-angular/src/app/components/payments-list/payments-list.component.ts
@@ -1,10 +1,13 @@
-import {Component, inject, Input, OnInit} from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { Payment } from "../../models/payment.model";
-import {PaymentsService} from "../../services/payments.service";
-import {Observable, Subscription} from "rxjs";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {CurrencyList} from "../../models/types";
-// import {MessageService} from "primeng/api";
+import { PaymentsService } from "../../services/payments.service";
+import { catchError, EMPTY, empty, Observable } from "rxjs";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { CurrencyList } from "../../models/types";
+import { MessageService } from 'primeng/api';
+import { HttpResponse, HttpStatusCode } from '@angular/common/http';
+import { ParseError } from '@angular/compiler';
+
 
 
 @Component({
@@ -12,10 +15,10 @@ import {CurrencyList} from "../../models/types";
   templateUrl: './payments-list.component.html',
   styleUrls: ['./payments-list.component.css']
 })
-export class PaymentsListComponent implements OnInit{
-  paymentService=inject(PaymentsService);
-  paymentsList$? : Observable<Payment[]>;
-  payment$? : Subscription;
+export class PaymentsListComponent implements OnInit {
+  paymentService = inject(PaymentsService);
+  paymentsList$?: Observable<Payment[]>;
+  display = false;
 
   selectedCurrency: any = null;
   currencies = CurrencyList.concat();
@@ -25,36 +28,46 @@ export class PaymentsListComponent implements OnInit{
     this.getAll();
   }
 
-  deletePayment(id : string){
+  deletePayment(id: string) {
     var numberValue = Number(id);
-    this.payment$ = this.paymentService.deletePayment(numberValue).subscribe(()=>this.getAll());
-    this.ngOnInit();
+    this.paymentService.deletePayment(numberValue).subscribe({
+      next: () => { this.getAll() },
+      error: (err: HttpResponse<HttpStatusCode>) => { this.messageService.add({ severity: 'error', summary: 'Error'}) }
+    })
   }
 
   getAll() {
-    this.paymentsList$ = this.paymentService.getAll();
+    this.paymentsList$ = this.paymentService.getAll().pipe(catchError((err: HttpResponse<HttpStatusCode>) => {
+      this.messageService.add({ severity: 'error', summary: 'Error'}); 
+      throw err;
+    }))
   }
 
-  updatePayment(id : string, payment : Payment){
+  updatePayment(id: string, payment: Payment) {
     var numberValue = Number(id);
-    this.payment$ = this.paymentService.updatePayment(numberValue, payment).subscribe(()=>this.getAll());
+    this.paymentService.updatePayment(numberValue, payment).subscribe({
+      next: () => { this.getAll() },
+      error: (err: HttpResponse<HttpStatusCode>) => { this.messageService.add({ severity: 'error', summary: 'Error' }) }
+    })
   }
 
-  createNewPayment(payment : Payment){
-    this.payment$ = this.paymentService.createNewPayment(payment).subscribe(()=>this.getAll());
-
+  createNewPayment(payment: Payment) {
+    this.paymentService.createNewPayment(payment).subscribe({
+      next: () => { this.getAll() },
+      error: (err: HttpResponse<HttpStatusCode>) => { this.messageService.add({ severity: 'error', summary: 'Error'}) }
+    })
   }
 
   public form: FormGroup = this.formBuilder.group({
-    payerEmail: ['', [Validators.required]],
+    payerEmail: ['', [Validators.required, Validators.email]],
     currency: ['', [Validators.required]],
-    amount: ['', [Validators.required]]
+    amount: ['0', [Validators.required, Validators.min(0)]]
   });
 
   constructor(
-    private formBuilder: FormBuilder
-    // private messageService: MessageService
-  ) {  }
+    private formBuilder: FormBuilder,
+    private messageService: MessageService
+  ) { }
 
 
 }
